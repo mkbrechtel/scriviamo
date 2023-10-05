7a7aba0c87d744907b7b5d98f7d5936b0cffe582
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-10-05 15:23:15 +0200

    got collab working!!!

--- src/lib/MarkdownEditor.svelte
+++ src/lib/MarkdownEditor.svelte
@@ -2,23 +2,38 @@
 import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core';
 import { commonmark } from '@milkdown/preset-commonmark';
 import { nord } from '@milkdown/theme-nord';
+import { WebsocketProvider } from 'y-websocket';
+import { Doc } from 'yjs';
+import { collab, collabServiceCtx } from '@milkdown/plugin-collab';
 
 function editor(dom) {
-  // to obtain the editor instance we need to store a reference of the editor.
+  const doc = new Doc();
+  const wsProvider = new WebsocketProvider('ws://localhost:3980/collaboration', 'test', doc);
+ 
   const MakeEditor = Editor.make()
     .config((ctx) => {
       ctx.set(rootCtx, dom);
     })
     .config(nord)
     .use(commonmark)
+    .use(collab)
     .create();
+
   MakeEditor.then((editor) => {
-    // here you have access to the editor instance.
-    
-    // const exampleContent = "# Hello World!";
-    // editor.action(replaceAll(exampleContent));
+    editor.action((ctx) => {
+      const collabService = ctx.get(collabServiceCtx);
+
+      collabService
+        // bind doc and awareness
+        .bindDoc(doc)
+        .setAwareness(wsProvider.awareness)
+        // connect yjs with milkdown
+        .connect();
+    });
+
   })
 }
+
 </script>
 
 <div use:editor />
