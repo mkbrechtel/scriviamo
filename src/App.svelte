f262eebd053d30b8030eb980bcfa52138c826a86
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2023-10-05 14:10:23 +0200

    insert milkdown editor

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,12 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import MarkdownEditor from './lib/MarkdownEditor.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1 class="text-3xl font-bold underline">scriviamo</h1>
 
   <div>
-    <Counter />
+    <MarkdownEditor />
   </div>
 
 </main>
---
+++ src/lib/MarkdownEditor.svelte
@@ -0,0 +1,24 @@
+<script>
+import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core';
+import { commonmark } from '@milkdown/preset-commonmark';
+import { nord } from '@milkdown/theme-nord';
+
+function editor(dom) {
+  // to obtain the editor instance we need to store a reference of the editor.
+  const MakeEditor = Editor.make()
+    .config((ctx) => {
+      ctx.set(rootCtx, dom);
+    })
+    .config(nord)
+    .use(commonmark)
+    .create();
+  MakeEditor.then((editor) => {
+    // here you have access to the editor instance.
+    
+    // const exampleContent = "# Hello World!";
+    // editor.action(replaceAll(exampleContent));
+  })
+}
+</script>
+
+<div use:editor />
